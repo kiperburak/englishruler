a11e85d7e14dd0dc45aede1d4762a40adc86b80b
halimwastaken <halim_gulesen@hotmail.com>
2020-11-07 20:34:17 +0300

    generateLines recursive

--- englishruler.py
+++ englishruler.py
@@ -1,20 +1,23 @@
-numberOfTickPerInch = 5
+numberOfTickPerInch = 4
 ticks = 2(numberOfTickPerInch - 1) - 1
 
 dict_tick = {}
 
 
 def generateTick(tick_1, number_1):
-    dict_tick[round(tick_1/2)] = round(number_1-1)
-    number_1 = number_1 - 1
-    if (number_1 > 0):
-        generateTick(2(number_1 - 1) - 1, number_1)
+    if number_1-1 > 0 and round(tick_1/2) > 0:
+        dict_tick[round(tick_1/2)] = round(number_1-1)
+        number_1 = number_1 - 1
+        if (number_1 > 0):
+            generateTick(2(number_1 - 1) - 1, number_1)
 
+def getmod(number):
+    for i in dict_tick.keys():
+        if i != 0 and i % number == 0:
+            return number
+    return -1
 
+for i in range(1, ticks):
+    generateTick(2**(numberOfTickPerInch - 1) - 1, numberOfTickPerInch)
 
-for i in range(2):
-    generateTick(2(numberOfTickPerInch - 1) - 1, numberOfTickPerInch)
-
-for i in range(ticks):
-    for j in range(dict_tick.keys().count()):
-        print(dict_tick.get()
+print(dict_tick)
