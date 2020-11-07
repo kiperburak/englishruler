578cd1c600a31eaf88663539dbbb22b3da7148f9
kyperbeast <47918397+kyperbeast@users.noreply.github.com>
2020-11-07 20:36:05 +0300

    final commit

--- englishruler.py
+++ englishruler.py
@@ -1,23 +1,35 @@
-numberOfTickPerInch = 4
-ticks = 2(numberOfTickPerInch - 1) - 1
+def generateTick(tick, number, tick_dictionary):
+    if number-1 > 0 and round(tick/2) > 0:
+        tick_dictionary[round(tick/2)] = round(number-1)
+        number = number - 1
+        if (number > 0):
+            generateTick(2**(number - 1) - 1, number, tick_dictionary)
+    return tick_dictionary
 
-dict_tick = {}
-
-
-def generateTick(tick_1, number_1):
-    if number_1-1 > 0 and round(tick_1/2) > 0:
-        dict_tick[round(tick_1/2)] = round(number_1-1)
-        number_1 = number_1 - 1
-        if (number_1 > 0):
-            generateTick(2(number_1 - 1) - 1, number_1)
-
-def getmod(number):
-    for i in dict_tick.keys():
-        if i != 0 and i % number == 0:
-            return number
+def getmod(number, tick_dictionary):
+    for i in tick_dictionary.keys():
+        if number % i == 0:
+            return tick_dictionary.get(i)
     return -1
 
-for i in range(1, ticks):
-    generateTick(2**(numberOfTickPerInch - 1) - 1, numberOfTickPerInch)
+def printLine(tick_dictionary, numberOfTickPerInch, rulerSize):
+    for j in range(0, rulerSize):
+        print("-"*numberOfTickPerInch, j)
+        for i in range(1, ticks+1):
+            mode = getmod(i, tick_dictionary)
+            if i in tick_dictionary.keys():
+                print("-"*tick_dictionary.get(i))
+            elif mode != -1:
+                print("-"*mode)
+            else:
+                print("-")
+    print("-"*numberOfTickPerInch, rulerSize)
 
-print(dict_tick)
+if __name__ == '__main__':
+    rulerSize = 2
+    numberOfTickPerInch = 5
+    ticks = 2**(numberOfTickPerInch - 1) - 1
+    tick_dictionary = {}
+
+    generateTick(ticks, numberOfTickPerInch, tick_dictionary)
+    printLine(tick_dictionary, numberOfTickPerInch, rulerSize)
