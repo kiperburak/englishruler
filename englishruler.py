a55f052f18d2ee4119e791664a5f6fc83b22d869
halimwastaken <halim_gulesen@hotmail.com>
2020-11-07 20:27:48 +0300

    improvements

--- englishruler.py
+++ englishruler.py
@@ -1,11 +1,17 @@
 numberOfTickPerInch = 5
-ticks = 2**(numberOfTickPerInch - 1) - 1
+ticks = 2(numberOfTickPerInch - 1) - 1
 
 dict_tick = {}
 
 
-
 def generateTick(tick_1, number_1):
     dict_tick[round(tick_1/2)] = round(number_1-1)
+    if (number_1 > 0)
+    generateTick(2(number_1 - 2) - 1, number_1-1)
+
+
+
+for i in range(2):
+    generateTick(2(numberOfTickPerInch - 1) - 1, numberOfTickPerInch)
 
 print(dict_tick)
