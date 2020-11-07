a38f80caa45bf4ef8cd8f8df72064fdb7268aafb
kyperbeast <47918397+kyperbeast@users.noreply.github.com>
2020-11-07 20:30:38 +0300

    creating lines with for

--- englishruler.py
+++ englishruler.py
@@ -1,11 +1,21 @@
 numberOfTickPerInch = 5
-ticks = 2**(numberOfTickPerInch - 1) - 1
+ticks = 2(numberOfTickPerInch - 1) - 1
 
 dict_tick = {}
 
 
-
 def generateTick(tick_1, number_1):
     dict_tick[round(tick_1/2)] = round(number_1-1)
+    number_1 = number_1 - 1
+    if (number_1 > 0):
+        generateTick(2(number_1 - 1) - 1, number_1)
 
-print(dict_tick)
+
+
+for i in range(2):
+    generateTick(2(numberOfTickPerInch - 1) - 1, numberOfTickPerInch)
+
+
+for i in range(ticks):
+    for j in range(dict_tick.keys().count()):
+        print(dict_tick.get()
