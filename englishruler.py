1ca96161c506c8a7bd7e200509f5b0cbdb3aae3f
kyperbeast <47918397+kyperbeast@users.noreply.github.com>
2020-11-08 16:40:35 +0300

    added some comment lines

--- englishruler.py
+++ englishruler.py
@@ -1,3 +1,7 @@
+'İki inch arası boşluk sayısına göre tickler bu fonksiyonda hesaplanıp ayrılır'
+'Mesela 5 tickli bi inch gösterimi düşünürsek iki inch birimi arasında(2**(5-1)-1)=15 boşluk olacaktır.
+'generateTick fonksiyonu da bu boşluklar arasının ortalarını bularak onları tick_dictionary içine atar.'
+'Örneğin 8:4,4:3,2:2... olacak şekilde'
 def generateTick(tick, number, tick_dictionary):
     if number-1 > 0 and round(tick/2) > 0:
         tick_dictionary[round(tick/2)] = round(number-1)
@@ -11,7 +15,7 @@ def getmod(number, tick_dictionary):
         if number % i == 0:
             return tick_dictionary.get(i)
     return -1
-
+'Görüldüğü üzere fonksiyon kendini çağırmaktadır(tick_directionary).Bu yüzden fonksiyon lineer recursion"a sahiptir.'
 def printLine(tick_dictionary, numberOfTickPerInch, rulerSize):
     for j in range(0, rulerSize):
         print("-"*numberOfTickPerInch, j)
@@ -24,7 +28,9 @@ def printLine(tick_dictionary, numberOfTickPerInch, rulerSize):
             else:
                 print("-")
     print("-"*numberOfTickPerInch, rulerSize)
-
+'Fonksiyonun karmaşıklık analizine bakar isek,görüldüğü üzere iki for döngüsünün değişkenleri bize karmaşıklığı vermektedir'
+'Bunlar da rulerSize ve ticks değişkenleridir(ticks değişkeni de numberofTickPerInch değişkenine bağlıdır.)'
+'O yüden karmaşıklık O(rulerSize*numberOfTickPerInch) olacaktır.'
 if __name__ == '__main__':
     rulerSize = 2
     numberOfTickPerInch = 5
